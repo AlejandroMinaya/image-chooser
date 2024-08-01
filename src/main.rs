c4c2808e1e289b71d8e07cdfd4f22596244c6ac4
Juan A <j.alejandrominaya@gmail.com>
2024-07-31 19:51:32 -0600

    feat: Setup window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::{
+    egui::{
+        ViewportBuilder,
+        Context,
+        CentralPanel,
+    },
+    Result,
+    run_native,
+    NativeOptions,
+    Frame
+};
+
+#[derive(Default)]
+struct ImageChooser;
+impl eframe::App for ImageChooser {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("klk");
+        });
+    }
+}
+
+fn main() -> Result {
+    let options = NativeOptions {
+        viewport: ViewportBuilder::default().with_inner_size([1440.0, 810.0]),
+        ..Default::default()
+    };
+
+    run_native("", options, Box::new(|cc| {
+        egui_extras::install_image_loaders(&cc.egui_ctx);
+        Ok(Box::<ImageChooser>::default())
+    }))
 }
