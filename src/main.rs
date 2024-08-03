4a7785e0f55b4d026b4745fa0639d31d515c5173
Juan A <j.alejandrominaya@gmail.com>
2024-08-02 23:09:27 -0600

    feat: Add support for PNG

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use eframe::{
         CentralPanel,
         Image,
         TopBottomPanel,
+        Layout,
+        Direction,
+        Align,
         Key,
         Ui
     },
@@ -16,8 +19,8 @@ use eframe::{
 use std::fs;
 use rand::{thread_rng, seq::SliceRandom};
 
-const WINNER_URI: &str = "bytes://winner_image.jpg";
-const OTHER_URI: &str = "bytes://other_image.jpg";
+const WINNER_URI: &str = "bytes://winner_image";
+const OTHER_URI: &str = "bytes://other_image";
 
 #[derive(Default)]
 struct ImageChooser {
@@ -32,15 +35,25 @@ fn load_image<'a>(entry: &'a fs::DirEntry, uri: &'a str) -> Image<'a> {
      return Image::from_bytes(uri.to_owned(), image_bytes)
 }
 
-fn render_image<'a>(ui: &'a mut Ui, entry: &'a fs::DirEntry, uri: &'a str) {
-    ui.vertical(|ui| {
-        ui.label(entry.path().display().to_string());
+fn render_image<'a>(ui: &'a mut Ui, entry: &'a fs::DirEntry, uri: &'a str, width: f32) {
+    ui.with_layout(Layout {
+        main_dir: Direction::TopDown,
+        main_align: Align::Center,
+        main_wrap: false,
+        main_justify: false,
+        cross_align: Align::Min,
+        cross_justify: false,
+    },|ui| {
+        ui.set_width(width);
+        ui.set_height(ui.available_height());
 
         let image = load_image(entry, uri)
-             .show_loading_spinner(true)
-             .shrink_to_fit();
+            .maintain_aspect_ratio(true)
+            .fit_to_exact_size([width, width].into());
 
         ui.add(image);
+        ui.label(entry.path().display().to_string());
+
     });
 }
 
@@ -52,7 +65,7 @@ impl ImageChooser {
             .map(|file_entry| file_entry.expect("Expected file"))
             .filter(|file_entry| {
                 match file_entry.path().extension() {
-                    Some(extension) => extension == "jpg",
+                    Some(extension) => extension == "jpg" || extension == "png",
                     None => false
                 }
             })
@@ -65,7 +78,6 @@ impl ImageChooser {
         if let Some(path) = file_picker.pick_folder() {
             self.load_image_list(path.display().to_string());
         }
-
     }
 }
 impl eframe::App for ImageChooser {
@@ -87,30 +99,40 @@ impl eframe::App for ImageChooser {
         });
 
         CentralPanel::default().show(ctx, |ui| {
+            let width = ui.available_width();
+            let half_width = ui.available_width() / 2.0;
+            ui.with_layout(Layout {
+                main_dir: Direction::LeftToRight,
+                main_align: Align::Center,
+                main_wrap: true,
+                main_justify: false,
+                cross_align: Align::Center,
+                cross_justify: false
+            },|mut ui| {
+                ui.set_width(width);
             /* === IMAGE COMPARISON === */
             if let Some(winner) = &self.winner {
 
                 let other_entry = self.images.last();
-                ui.horizontal(|mut ui| {
-                    // WINNER IMAGE
-                    render_image(&mut ui, winner, WINNER_URI);
+                // WINNER IMAGE
+                render_image(&mut ui, winner, WINNER_URI, half_width);
 
-                    // OTHER IMAGE
-                    if let Some(other_entry) = other_entry {
-                        render_image(&mut ui, other_entry, OTHER_URI);
-                    }
-                });
+                // OTHER IMAGE
+                if let Some(other_entry) = other_entry {
+                    render_image(&mut ui, other_entry, OTHER_URI, half_width);
+                }
             }
+            });
             /* ======================== */
             /* ===== KEYBOARD INPUTS ===== */
             if ctx.input(|i| i.key_released(Key::Enter)) && self.images.is_empty() {
                 self.trigger_file_explorer();
 
             }
-            if ctx.input(|i| i.key_released(Key::ArrowLeft)) {
+            if ctx.input(|i| i.key_released(Key::ArrowLeft) || i.key_released(Key::ArrowUp)) {
                 self.images.pop();
                 ui.ctx().forget_image(OTHER_URI);
-            } else if ctx.input(|i| i.key_released(Key::ArrowRight)) {
+            } else if ctx.input(|i| i.key_released(Key::ArrowRight) || i.key_released(Key::ArrowDown)) {
                 self.winner = self.images.pop();
                 ui.ctx().forget_image(WINNER_URI);
                 ui.ctx().forget_image(OTHER_URI);
